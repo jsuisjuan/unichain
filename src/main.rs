cd217952f9872ecccfd0c4e2b1626144a0ce1f5a
jsuisjuan <juanc.s.delima@gmail.com>
2024-10-31 20:29:57 -0300

    changing struct

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,68 @@
 use chrono::NaiveDate;
+use idgenerator::*;
+use std::process;
 
 #[derive(Debug)]
-enum DocumentStatus {
-    Pending,
-    Approved,
-    Rejected,
-    UnderReview
+enum FileType {
+    Pdf,
+    Docx,
+    Xls,
+    Txt,
+    Csv,
+    Pptx,
+    Jpg,
+    Png,
 }
 
 #[derive(Debug)]
-struct TransferDocument {
-    doc_name: String,
-    student_id: u32,
-    institution_origin: String,
-    institution_destination: String,
-    pages: u32,
-    issued_at: NaiveDate,
-    status: DocumentStatus
+struct File {
+    id: i64,
+    name: String,
+    file_type: FileType,
+    size: u64,
+    created: NaiveDate,
+    modified: NaiveDate,
+    accessed: Option<NaiveDate>,
+    owner: (i64, String, String),
+    people_with_access: Vec<(i64, String, String)>,
+    ipfs_hash: String,
+    onchain_txn_id: String,
+    download_permission: bool,
+    description: Option<String>
+}
+
+fn generate_id() -> i64 {
+    let options = IdGeneratorOptions::new().worker_id(1).worker_id_bit_len(6);
+    let _ = IdInstance::init(options).unwrap_or_else(|err| {
+        eprintln!("Problem on instancing id generator options: {err}");
+        process::exit(1);
+    });
+    IdInstance::next_id()
 }
 
 fn main() {
-    println!("Welcome to the UniChain!");
-    let issued_at: NaiveDate = NaiveDate::from_ymd_opt(2024, 10, 26)
-        .expect("Invalid Date");
-    let document: TransferDocument = TransferDocument {
-        doc_name: String::from("Histórico Acadêmico"),
-        student_id: 12345,
-        institution_origin: String::from("Universidade A"),
-        institution_destination: String::from("Universidade B"),
-        pages: 10,
-        issued_at,
-        status: DocumentStatus::Approved
+    println!("Welcome to your UniChain!");
+    // log my personal data
+    
+
+
+    let file = File {
+        id: generate_id(),
+        name: String::from("Example Document"),
+        file_type: FileType::Pdf,
+        size: 2048,
+        created: NaiveDate::from_ymd_opt(2024, 10, 26).expect("Invalid date"),
+        modified: NaiveDate::from_ymd_opt(2024, 10, 26).expect("Invalid date"),
+        accessed: None,
+        owner: (generate_id(), String::from("John Doe"), String::from("john.doe@gmail.com")),
+        people_with_access: vec![
+            (generate_id(), String::from("Jane Smith"), String::from("jane.smith@gmail.com")),
+            (generate_id(), String::from("Alice Johnson"), String::from("alice.johnson@outlook.com")),
+        ],
+        ipfs_hash: String::from("QmExampleIPFSHash"),
+        onchain_txn_id: String::from("0xExampleBlockchainTxnID"),
+        download_permission: true,
+        description: Some(String::from("Sample file for testing")),
     };
-    println!("{:#?}", document);
+    println!("{:#?}", file);
 }
