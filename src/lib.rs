efb9b536f6263228ceea6f0b77e86f1356881544
jsuisjuan <juanc.s.delima@gmail.com>
2024-10-31 22:21:41 -0300

    including some fs functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use chrono::NaiveDate;
+use serde::{Serialize, Deserialize};
+use std::fs::File as StdFile;
+use std::io::{Write, Read};
+use bincode;
 use idgenerator::*;
 use std::process;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum FileType {
     Pdf,
     Docx,
@@ -14,7 +18,7 @@ pub enum FileType {
     Png,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct File {
     pub id: i64,
     pub name: String,
@@ -38,4 +42,52 @@ pub fn generate_id() -> i64 {
         process::exit(1);
     });
     IdInstance::next_id()
+}
+
+pub fn load_files_from_file(path: &str) -> std::io::Result<Vec<File>> {
+    let mut file = match StdFile::open(path) {
+        Ok(f) => f,
+        Err(_) => return Ok(Vec::new())
+    };
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    let decoded: Vec<File> = bincode::deserialize(&buffer).expect("Failed to deserialize Vec<File>");
+    Ok(decoded)
+}
+
+pub fn save_files_to_file(files: &Vec<File>, path: &str) -> std::io::Result<()> {
+    let encoded: Vec<u8> = bincode::serialize(files).expect("Failed to serialize Vec<File>");
+    let mut file = StdFile::create(path)?;
+    file.write_all(&encoded)?;
+    Ok(())
+}
+
+pub fn add_file(file: File, path: &str) -> std::io::Result<()> {
+    let mut files = load_files_from_file(path)?;
+    files.push(file);
+    save_files_to_file(&files, path)
+}
+
+//Create
+pub fn create_file() {
+
+}
+
+//Read
+pub fn get_all_files() {
+
+}
+
+pub fn get_file() {
+
+}
+
+//Update
+pub fn modify_file() {
+
+}
+
+//Delete
+pub fn delete_file() {
+
 }
