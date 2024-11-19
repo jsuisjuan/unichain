11aad18477f35ff5441541ea684f36a2e08347f5
jsuisjuan <juanc.s.delima@gmail.com>
2024-11-19 15:37:04 -0300

    keeping with the cli

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,72 @@
-use std::io;
+use std::io::{self, Write};
+
+use unichain::{get_all_files, get_file, create_new_file, modify_file, delete_file};
+
+const PATH: &str = "../assets/";
 
 fn get_system_owner() -> (i64, String, String) {
-    (
-        2454826096558341,
-        String::from("Juan Carvalho Silva de Lima"),
-        String::from("juanc.s.delima@gmail.com"),
-    )
+    (2454826096558341, String::from("Juan Carvalho Silva de Lima"), String::from("juanc.s.delima@gmail.com"))
+}
+
+fn print_menu_options() {
+    println!("What do you want to do?\n");
+    println!("1. View list of stored files\n2. View a specific file\n3. Store a new file\n4. Update an existing file\n5. Move a file to trash\n0. Exit");
+}
+
+fn get_choosed_option() -> Result<u8, String> {
+    print!("Choose an option: ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut choosed_option = String::new();
+    io::stdin().read_line(&mut choosed_option).expect("Failed to read from stdin");
+    match choosed_option.trim().parse::<u8>() {
+        Ok(num) => Ok(num),
+        Err(_) => Err("Please enter a valid number between 0 and 5.".to_string())
+    }
+}
+
+fn get_files() {
+    get_all_files(PATH);
+}
+
+fn get_specific_file() {
+    let file_id = 3;
+    get_file(PATH, file_id);
+}
+
+fn store_new_file() {
+    // let file_data;
+    // create_new_file(file_data, PATH);
+}
+
+fn update_existing_file() {
+    // let updated_file;
+    let file_id = 3;
+    // modify_file(PATH, file_id, updated_file);
+}
+
+fn move_file_to_trash() {
+    let file_id = 3;
+    delete_file(PATH, file_id);
 }
 
 fn system_menu() {
-    println!("What do you want to do?");
-    println!("
-        1. View list of stored files\n
-        2. View a specific file\n
-        3. Store a new file\n
-        4. Update an existing file\n
-        5. Move a file to trash\n
-        0. Exit
-    ");
-    let choice: u8 = loop {
-        let mut choosed_option: String = String::new();
-        io::stdin().read_line(&mut choosed_option).expect("Failed to read from stdin");
-        match choosed_option.trim().parse::<u8>() {
-            Ok(option) if (0..=5).contains(&option) => break option,
+    print_menu_options();
+    loop {
+        match get_choosed_option() {
+            Ok(option) if (0..=5).contains(&option) => {
+                match option {
+                    0 => {println!("Exiting..."); break;},
+                    1 => get_files(),
+                    2 => get_specific_file(),
+                    3 => store_new_file(),
+                    4 => update_existing_file(),
+                    5 => move_file_to_trash(),
+                    _ => unreachable!()
+                }
+            },
             _ => println!("Please enter a valid number between 0 and 5.")
         }
-    };
-    match choice {
-        0 => println!("Exiting..."),
-        1 => println!("Viewing list of stored files..."),
-        2 => println!("Viewing a specific file..."),
-        3 => println!("Storing a new file..."),
-        4 => println!("Updating an existing file..."),
-        5 => println!("Moving a file to trash..."),
-        _ => unreachable!()
-    };
+    }
 }
 
 fn main() {
